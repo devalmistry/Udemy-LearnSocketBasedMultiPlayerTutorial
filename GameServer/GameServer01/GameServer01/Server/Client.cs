802c4f38564126b4bcc3d4f0ad72402386710542
devalmistry <dha_mistry@yahoo.co.in>
2022-10-17 19:56:17 +0530

    Lecture 5 Message Class Entrance

---
+++ GameServer/GameServer01/GameServer01/Server/Client.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+
+namespace GameServer01.Server
+{
+    class Client
+    {
+        private Socket clientSocket;
+        private Server server;
+
+        public Client(Socket clientSocket, Server server)
+        {
+            this.clientSocket = clientSocket;
+            this.server = server;
+        }
+
+        public void start()
+        {
+            clientSocket.BeginReceive(null, 0, 0, SocketFlags.None, ReceiveCallBack, null);
+        }
+
+        private void ReceiveCallBack(IAsyncResult ar)
+        {
+            int count = clientSocket.EndReceive(ar);
+            if (count==0)
+            {
+                Close();
+            }
+            start();
+        }
+
+        private void Close()
+        {
+            if (clientSocket!=null)
+            {
+                clientSocket.Close();
+            }
+            server.RemoveClient(this);
+        }
+    }
+}
