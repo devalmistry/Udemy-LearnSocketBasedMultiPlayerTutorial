816bc2fe342d14e8a2a142479a7ea77d09d85ea1
devalmistry <dha_mistry@yahoo.co.in>
2022-10-17 18:20:54 +0530

    Second Lecture

--- Socket Chat/SimpleChatClient/Program.cs
+++ Socket Chat/SimpleChatClient/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,25 @@ namespace SimpleChatClient
     {
         static void Main(string[] args)
         {
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            clientSocket.Connect(new IPEndPoint(IPAddress.Parse("192.168.29.220"),88));
+
+            //Reading The Hello Message
+
+            byte[] data = new byte[1024];
+            int count = clientSocket.Receive(data);
+
+            string msgReceive = Encoding.UTF8.GetString(data,0,count);
+
+            Console.WriteLine(msgReceive);
+
+            //Sending Message to the Server
+
+            string msgSend = Console.ReadLine();
+            clientSocket.Send(Encoding.UTF8.GetBytes(msgSend));
+
+            Console.ReadKey();
+            clientSocket.Close();
         }
     }
 }
