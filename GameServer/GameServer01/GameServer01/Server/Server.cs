f13b9c9e5f1ad89651587942e11b314e350a89fc
devalmistry <dha_mistry@yahoo.co.in>
2022-10-17 19:06:08 +0530

    Lecture 04 ServerSideCleint Entrance

---
+++ GameServer/GameServer01/GameServer01/Server/Server.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace GameServer01.Server
+{
+    class Server
+    {
+        private IPEndPoint iPEndPoint;
+        Socket serverSocket;
+        public Server(String ipStr, int port)
+        {
+            iPEndPoint = new IPEndPoint(IPAddress.Parse(ipStr), port);
+        }
+
+        public void Start() {
+              serverSocket = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            serverSocket.Bind(iPEndPoint);
+            serverSocket.Listen(0);
+            serverSocket.BeginAccept(AcceptCallBack,null);
+        }
+
+        private void AcceptCallBack(IAsyncResult ar)
+        {
+            Socket clientSocket = serverSocket.EndAccept(ar);
+        }
+    }
+}
