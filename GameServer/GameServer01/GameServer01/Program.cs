70bdce846ccc6435c4ee9723a4544cc9ff599c85
devalmistry <dha_mistry@yahoo.co.in>
2022-10-19 17:53:01 +0530

    Lecture 14 ClientManager

--- GameServer/GameServer01/GameServer01/Program.cs
+++ GameServer/GameServer01/GameServer01/Program.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using GameServer01.Servers;
+
 namespace GameServer01
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Server server = new Server("127.0.0.1", 6688);
+            server.Start();
         }
     }
 }
--- UdemeyGameClient/Assets/Scripts/Net/ClientManager.cs
+++ UdemeyGameClient/Assets/Scripts/Net/ClientManager.cs
@@ -1,16 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using UnityEngine;
 
 public class ClientManager : BaseManager
 {
+
+    private const string IP = "127.0.0.1";
+    private const int PORT = 6688;
+    Socket clientSocket;
+
+
     public override void OnDestroy()
     {
+        clientSocket = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+        try
+        {
+            clientSocket.Connect(IP, PORT);
+        }
+        catch (Exception e)
+        {
 
+            Debug.Log (e);
+        }
     }
 
     public override void OnInit()
     {
+        try
+        {
+            clientSocket.Close();
+        }
+        catch (Exception e)
+        {
 
+            Debug.Log(e);
+        }
     }
 }
