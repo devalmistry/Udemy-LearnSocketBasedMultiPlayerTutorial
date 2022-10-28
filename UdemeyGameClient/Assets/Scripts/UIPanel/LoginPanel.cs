18ce359edcea38b45246c6132970cf6ef23b5831
devalmistry <dha_mistry@yahoo.co.in>
2022-10-28 19:24:14 +0530

    Lecture 28. Close Button

--- UdemeyGameClient/Assets/Scripts/UIPanel/LoginPanel.cs
+++ UdemeyGameClient/Assets/Scripts/UIPanel/LoginPanel.cs
@@ -2,9 +2,13 @@ using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoginPanel : BasePanel
 {
+
+    private Button closeButton;
+
     public override void OnEnter()
     {
         transform.localScale = Vector3.zero;
@@ -12,5 +16,13 @@ public class LoginPanel : BasePanel
         transform.localPosition = new Vector3(1000, 0, 0);
 
         transform.DOLocalMove(Vector3.zero,0.5f);
+
+        closeButton = transform.Find("CloseButton").GetComponent<Button>();
+        closeButton.onClick.AddListener(OnCloseClick);
+    }
+
+    private void OnCloseClick() {
+        transform.DOScale(0, 0.3f);
+        transform.DOLocalMove(new Vector3(1000, 0, 0),0.3f);
     }
 }
